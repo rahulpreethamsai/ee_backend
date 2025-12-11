e8202620404c0b2bc4b185b4b7ca66cc18349634
rahulpreethamsai <chrahulpreethamsai@gmail.com>
2025-12-11 12:44:09 +0530

    first

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import router from './routes/authRoute.js';
+import connectDB from './config/db.js';
+
+const app = express();
+
+dotenv.config();
+connectDB();
+
+app.use(cors({
+    "origin": "http://localhost:5173"
+}
+))
+app.use(express.json());
+
+app.use('/api/auth', router);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port , () => {
+    console.log(`SHU SHU SERVER STARTED ON ${port}`)
+});
